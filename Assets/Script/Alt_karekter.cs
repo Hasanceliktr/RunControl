1ac7281c19249548208ae2d4a8903e4383114511
Hasan Çelik <hasancelikdeveloper@gmail.com>
2022-07-31 22:02:19 +0300

    Karakteri takip etmeme sorunu dezeltildi. Bazı yazım yanlışları giderildi.

--- Assets/Script/Alt_karekter.cs
+++ Assets/Script/Alt_karekter.cs
@@ -4,25 +4,25 @@ using UnityEngine;
 using UnityEngine.AI;
 public class Alt_karekter : MonoBehaviour
 {
-    GameObject target;
+    GameObject Target;
     NavMeshAgent _NavMesh;
     void Start()
     {
         _NavMesh = GetComponent<NavMeshAgent>();
-        target = GameObject.FindWithTag("GameManager").GetComponent<Gamemaneger>().VarisNoktasi;
+        Target = GameObject.FindWithTag("GameManager").GetComponent<Gamemaneger>().VarisNoktasi;
     }
 
     // Update is called once per frame
-    private void lateUpdate()
+    private void LateUpdate()
     {
 
-        _NavMesh.SetDestination(target.transform.position);
+        _NavMesh.SetDestination(Target.transform.position);
     }
 
     private void OnTriggerEnter (Collider other)
     
     {
-        if (other.CompareTag("iðneliKutu")) 
+        if (other.CompareTag("igneliKutu")) 
         
         {
             GameObject.FindWithTag("GameManager").GetComponent<Gamemaneger>().AnlikKarakterSayisi--;
