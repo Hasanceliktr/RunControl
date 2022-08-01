1b38f7a10497e296bc7ef30800aaefb55b2e8119
Hasan Çelik <hasancelikdeveloper@gmail.com>
2022-08-01 22:16:01 +0300

    Karakter.cs sorunu düzeltildi

--- Assets/Script/Karakter.cs
+++ Assets/Script/Karakter.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Karekter : MonoBehaviour
+public class Karakter : MonoBehaviour
 {
     public GameManeger _GameManeger;
     private void FixedUpdate()
