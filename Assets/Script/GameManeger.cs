239f8011690bb17ff9c9f6e7746dfd6782289f04
Zeynep\Kayhan <kayhancelikdeveloper@gmail.com>
2022-08-02 01:36:03 +0300

    Matematiksel İşlemler

--- Assets/Script/GameManeger.cs
+++ Assets/Script/GameManeger.cs
@@ -15,26 +15,10 @@ public class GameManeger : MonoBehaviour
 
     }
 
-    // Update is called once per frametest
+    
     void Update()
     {
-        //    if (Input.GetKeyDown(KeyCode.A))
-
-        //        foreach (var item in Karakterler)
-        //        {
-
-        //            if (!item.activeInHierarchy)
-        //            {
-
-        //                item.transform.position = DogmaNoktasý.transform.position;
-        //                item.SetActive(true);
-        //                AnlikKarakterSayisi++;
-        //                break;
-
-        //            }
-
-
-        //        }
+       
     }
 
     public void AdamYonetimi(string islemturu, int Gelensayi, Transform Pozisyon)
@@ -42,140 +26,27 @@ public class GameManeger : MonoBehaviour
         switch (islemturu)
         {
             case "Carpma":
-               Kutuphane.Carpma(Gelensayi,Karakterler,Pozisyon);
-                
-                
+                Matematiksel_iþlemler.Carpma(Gelensayi, Karakterler, Pozisyon);
                 break;
 
             case "Toplama":
-                int sayi2 = 0;
-                foreach (var item in Karakterler)
-                {
-                    if (sayi2 < 3)
-                    {
-                        if (!item.activeInHierarchy)
-                        {
-
-                            item.transform.position = Pozisyon.position;
-                            item.SetActive(true);
-                            sayi2++;
-
-
-                        }
-                    }
-                    else
-                    {
-                        sayi2 = 0;
-                        break;
-                    }
-
-
-                }
-                AnlikKarakterSayisi += 3;
-
-
+                Matematiksel_iþlemler.Toplama(Gelensayi, Karakterler, Pozisyon);
                 break;
 
-            case "Cikarma":
-
-
-                if (AnlikKarakterSayisi < 4)
-                {
-                    foreach (var item in Karakterler)
-                    {
-                        item.transform.position = Vector3.zero;
-                        item.SetActive(false);
-                    }
-                    AnlikKarakterSayisi = 1;
-
-                }
-                else
-                {
-                int sayi3 = 0;
-                foreach (var item in Karakterler)
-                {
-                    if (sayi3 != 4)
-                    {
-                        if (item.activeInHierarchy)
-                        {
-
-                            item.transform.position = Vector3.zero;
-                            item.SetActive(false);
-                            sayi3++;
-
-
-                        }
-                    }
-                    else
-                    {
-                        sayi3 = 0;
-                        break;
-                    }
-
-                }
-                AnlikKarakterSayisi -= 4;
-
-        }
-
+            case "Cýkarma":
+                Matematiksel_iþlemler.Cýkarma(Gelensayi, Karakterler);
                 break;
+
 
             case "Bolme":
-
-
-                if (AnlikKarakterSayisi < 2)
-                {
-                    foreach (var item in Karakterler)
-                    {
-                        item.transform.position = Vector3.zero;
-                        item.SetActive(false);
-                    }
-                    AnlikKarakterSayisi = 1;
-
-                }
-                else
-                {
-
-                    int bolen = AnlikKarakterSayisi /2;
-
-                    int sayi3 = 0;
-                    foreach (var item in Karakterler)
-                    {
-                        if (sayi3 != bolen)
-                        {
-                            if (item.activeInHierarchy)
-                            {
-
-                                item.transform.position = Vector3.zero;
-                                item.SetActive(false);
-                                sayi3++;
-
-
-                            }
-                        }
-                        else
-                        {
-                            sayi3 = 0;
-                            break;
-                        }
-
-                    }
-                    if (AnlikKarakterSayisi % 2 == 0)
-                    {
-                        AnlikKarakterSayisi /= 2;
-                        AnlikKarakterSayisi++;
-                    }
-
-                }
-
+                Matematiksel_iþlemler.Bolme(Gelensayi, Karakterler);
                 break;
 
-
-
-
-
-
         }
+
+
     }
+  
 }
         
 
--- Assets/Script/Kutuphane.cs
+++ Assets/Script/Kutuphane.cs
@@ -9,7 +9,7 @@ namespace olcay
 
 
 
-    public class Kutuphane : MonoBehaviour
+    public class Matematiksel_iþlemler : MonoBehaviour
     {
     
     
@@ -41,13 +41,147 @@ namespace olcay
 
 
         }
-    
-    
-    
-    
-    
+
+        public static void Toplama(int GelenSayi, List<GameObject> Karakterler, Transform Pozisyon)
+        {
+            int sayi2 = 0;
+            foreach (var item in Karakterler)
+            {
+                if (sayi2 < GelenSayi)
+                {
+                    if (!item.activeInHierarchy)
+                    {
+
+                        item.transform.position = Pozisyon.position;
+                        item.SetActive(true);
+                        sayi2++;
+
+
+                    }
+                }
+                else
+                {
+                    sayi2 = 0;
+                    break;
+                }
+
+
+            }
+            GameManeger.AnlikKarakterSayisi += GelenSayi;
+
+
+        }
+
+        public static void Cýkarma(int GelenSayi, List<GameObject> Karakterler)
+        {
+
+            if (GameManeger.AnlikKarakterSayisi < GelenSayi)
+            {
+                foreach (var item in Karakterler)
+                {
+                    item.transform.position = Vector3.zero;
+                    item.SetActive(false);
+                }
+                GameManeger.AnlikKarakterSayisi = 1;
+
+            }
+            else
+            {
+                int sayi3 = 0;
+                foreach (var item in Karakterler)
+                {
+                    if (sayi3 != GelenSayi)
+                    {
+                        if (item.activeInHierarchy)
+                        {
+
+                            item.transform.position = Vector3.zero;
+                            item.SetActive(false);
+                            sayi3++;
+
+
+                        }
+                    }
+                    else
+                    {
+                        sayi3 = 0;
+                        break;
+                    }
+
+                }
+                GameManeger.AnlikKarakterSayisi -= GelenSayi;
+
+            }
+
+
+        }
+
+        public static void Bolme(int GelenSayi, List<GameObject> Karakterler)
+        {
+
+            if (GameManeger.AnlikKarakterSayisi < 2)
+            {
+                foreach (var item in Karakterler)
+                {
+                    item.transform.position = Vector3.zero;
+                    item.SetActive(false);
+                }
+                GameManeger.AnlikKarakterSayisi = 1;
+
+            }
+            else
+            {
+
+                int bolen = GameManeger.AnlikKarakterSayisi / GelenSayi;
+
+                int sayi3 = 0;
+                foreach (var item in Karakterler)
+                {
+                    if (sayi3 != bolen)
+                    {
+                        if (item.activeInHierarchy)
+                        {
+
+                            item.transform.position = Vector3.zero;
+                            item.SetActive(false);
+                            sayi3++;
+
+
+                        }
+                    }
+                    else
+                    {
+                        sayi3 = 0;
+                        break;
+                    }
+
+                }
+
+                if (GameManeger.AnlikKarakterSayisi % GelenSayi == 0)
+                {
+
+                    GameManeger.AnlikKarakterSayisi /= GelenSayi;
+
+
+                }
+                else if (GameManeger.AnlikKarakterSayisi % GelenSayi == 1)
+                {
+                    GameManeger.AnlikKarakterSayisi /= GelenSayi;
+                    GameManeger.AnlikKarakterSayisi++;
+
+                }
+                else if (GameManeger.AnlikKarakterSayisi % GelenSayi == 2)
+                {
+                   GameManeger.AnlikKarakterSayisi /= GelenSayi;
+                   GameManeger.AnlikKarakterSayisi +=2;
+                }
+
+
+                
+
+            }
+
+        }
     }
    
-        
-    
 }
