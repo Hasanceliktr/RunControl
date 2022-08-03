e3dbdded37f278bbeb0e76461352516ccfc58093
Kayhan Çelik <kayhancelikdeveloper@gmail.com>
2022-08-03 23:48:14 +0300

    İsimlendirme hatası düzeltildi.

--- Assets/Pervane.cs
+++ Assets/Pervane.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pervane1 : MonoBehaviour
+public class Pervane : MonoBehaviour
 {
     public Animator Animator;
 
