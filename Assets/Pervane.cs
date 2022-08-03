69864d3c2258db33e62be295f192801f764d6209
Kayhan Çelik <kayhancelikdeveloper@gmail.com>
2022-08-04 01:19:11 +0300

    rüzgar engeli yapıldı yanlız hata veriyor ve rüzgar çekmiyor şuan. hatalara bakarrmısın

--- Assets/Pervane.cs
+++ Assets/Pervane.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public class Pervane : MonoBehaviour
 {
-    public Animator Animator;
-
+    public Animator _Animator;
+    public float BeklemeSuresi;
+    public BoxCollider _Ruzgar;
     public void AnimasyonDurumu(string durum)
     {
+        if (durum == "true")
+        {
+            _Animator.SetBool("Calistir", true);
+            _Ruzgar.enabled = true;
+        }
+        else
+        {
+            _Animator.SetBool("Calistir", false);
+            StartCoroutine(AnimasyonTetikle());
+            _Ruzgar.enabled = false;
 
-
+        }
+            
+        
+         
     }
 
+    IEnumerator AnimasyonTetikle()
+    {
+
+        yield return new WaitForSeconds(BeklemeSuresi);
+        AnimasyonDurumu("true");
+        
+    }
 
 }
--- Assets/Script/Alt_karekter.cs
+++ Assets/Script/Alt_karekter.cs
@@ -41,6 +41,18 @@ public class Alt_karekter : MonoBehaviour
             gameObject.SetActive(false);
 
         }
+
+        if (other.CompareTag("Pervaneigneler"))
+
+        {
+            Vector3 yeniPoz = new Vector3(transform.position.x, .23f, transform.position.z);
+
+            GameObject.FindWithTag("GameManager").GetComponent<GameManeger>().YokolmaEfektiOlustur(yeniPoz);
+            gameObject.SetActive(false);
+
+        }
+
+
     }
 
 }
