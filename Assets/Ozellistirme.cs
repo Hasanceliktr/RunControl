ba73d88f713eedf8652407bb62a9c7a992b2d2ce
Kayhan Çelik <kayhancelikdeveloper@gmail.com>
2022-08-12 02:12:15 +0300

    ÖZELLEŞTİRME MENUSÜ YAPILDI 1 VE 2

---
+++ Assets/Ozellistirme.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Olcay;
+
+public class Ozellistirme : MonoBehaviour
+{
+    public Text PuanText;
+    public GameObject[] Sapkalar;
+    public GameObject[] Sopalar;
+    public Material[] Materyaller;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
